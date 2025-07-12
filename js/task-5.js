e320db8d8f8a595fa0c1ffc4983947c6e28f661b
Viktoriia Popel <xructa2009@gmail.com>
2025-07-12 21:06:42 +0200

    fix task 5 js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const groundColor = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  groundColor.textContent = randomColor;
+});
